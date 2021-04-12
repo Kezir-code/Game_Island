9a98c95ffbdd8aff0067ed965fe141f5e4d3e8d6
Kezir <kezir2@gmail.com>
2021-04-12 19:13:47 +0200

    Game_Island_2019
    wersja Unity 2019

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,4 +15,5 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
 }
