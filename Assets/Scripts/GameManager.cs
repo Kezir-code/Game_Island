115b55ad0ce288ebac5f22f18397844a4e18448e
Kamilaburz <nsz.burzynska@gmail.com>
2021-04-13 19:58:06 +0200

    Podstawowe_zmiennie
    Podstawowe zmiennie

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,55 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static int kamien;
+    public static int drewno;
+    public static int woda;
+    public static int dzien = 0;
+//    0 - rano
+//    1 - popoludnie
+//    2 - wieczor
+    public int pora_dnia = 0; 
+
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
+    public void Zmiana_Pory_Dnia()
+    {
+            
+            pora_dnia = (pora_dnia + 1) % 3;    
+        
+    }
 
+    public void Zmiana_kamien(int kamien_update)
+    {
+        int result = kamien + kamien_update;
+        if(result >= 0)
+        {
+            kamien = result;
+        }
+    }
+
+     public void Zmiana_wody(int woda_update)
+    {
+        int result = woda + woda_update;
+        if (result >= 0)
+        {
+            woda = result;
+        }
+    }
+    public void Zmiana_drewno(int drewno_update)
+    {
+        int result = drewno + drewno_update;
+        if (result >= 0)
+        {
+            drewno = result;
+        } 
+    }
+    public void Zmiana_dnia()
+    {
+        dzien++;
     }
 
     // Update is called once per frame
