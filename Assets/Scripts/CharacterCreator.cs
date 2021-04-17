f6e75d6993129654e16e08739d7aac70b58135b1
odiihinia <odiihinia@gmail.com>
2021-04-17 23:21:27 +0200

    Postacie ze skryptem
    Dodałem 19 tic taków gdzie każdy ma ten sam skrypt, który przydziela im imie/nazwisko, isAlive oraz lvl. Zamienilem też w branchu projectSettings na moje, więc błędy mogą być jeżeli cos waznego się zmieniło. Materials.meta też uległo zmianie i dodałem kilka tekstur na niektóre tictaki (w szczególności programistów)

---
+++ Assets/Scripts/CharacterCreator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCreator : MonoBehaviour
+{
+    public new string name;
+    public string surname;
+    public bool isAlive;
+    public int lvl;
+}
