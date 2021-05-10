a57ccb3bd49f7cfa671b81514e7ff93539317a23
molendzik <molendzik@gmail.com>
2021-05-10 21:52:21 +0200

    Dodano tymczasowe UI dla obsługi budynków i pracowników
    Dodany tymczasowy template budynku oraz funkcje, które (na razie) zmieniają tekst na przyciskach, aby było widać ich działanie. Template budynku będzie można później wykorzystać (zakładając dodanie list osób) do przydzielania osób z listy.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public GameObject guzik;
     public int liczbaGuzików;
+    private int aktuanieWybraneMiejsceWBudynku;
+    public Text opisMiejsceWBudynku1;
+    public Text opisMiejsceWBudynku2;
+    private Text osobaWybranaText;
 
+    
     void Start()
     {
-        dodajPrzyciski(liczbaGuzików);
+        //dodajPrzyciski(liczbaGuzików);
     }
 
 
@@ -18,6 +24,27 @@ public class UIManager : MonoBehaviour
         
     }
 
+    public void ZmienTextWMiejscuPracy (Text nazwisko)
+    {
+        if (aktuanieWybraneMiejsceWBudynku == 1)
+        {
+            opisMiejsceWBudynku1.text = "miejsce 1: " + nazwisko;
+        }
+        else if (aktuanieWybraneMiejsceWBudynku == 2)
+        {
+            opisMiejsceWBudynku2.text = "miejsce 2: " + nazwisko;
+        }
+    }
+
+    public void WyborMiejscaWBudynku1()
+    {
+        aktuanieWybraneMiejsceWBudynku = 1;
+    }
+
+    public void WyborMiejscaWBudynku2()
+    {
+        aktuanieWybraneMiejsceWBudynku = 2;
+    }
 
     void dodajPrzyciski(int liczbaPrzycisków)
     {
