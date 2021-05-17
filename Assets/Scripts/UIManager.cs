9b82275991ba24260b1fdb0b562f47cf952ec987
Kezir <75311317+Kezir-code@users.noreply.github.com>
2021-05-17 14:08:46 +0200

    zmiana wyglądu main

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject guzik;
+    public int liczbaGuzików;
+    private int aktuanieWybraneMiejsceWBudynku;
+    public Text opisMiejsceWBudynku1;
+    public Text opisMiejsceWBudynku2;
+    private Text osobaWybranaText;
+
+    
+    void Start()
+    {
+        //dodajPrzyciski(liczbaGuzików);
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void ZmienTextWMiejscuPracy (Text nazwisko)
+    {
+        if (aktuanieWybraneMiejsceWBudynku == 1)
+        {
+            opisMiejsceWBudynku1.text = "miejsce 1: " + nazwisko;
+        }
+        else if (aktuanieWybraneMiejsceWBudynku == 2)
+        {
+            opisMiejsceWBudynku2.text = "miejsce 2: " + nazwisko;
+        }
+    }
+
+    public void WyborMiejscaWBudynku1()
+    {
+        aktuanieWybraneMiejsceWBudynku = 1;
+    }
+
+    public void WyborMiejscaWBudynku2()
+    {
+        aktuanieWybraneMiejsceWBudynku = 2;
+    }
+
+    void dodajPrzyciski(int liczbaPrzycisków)
+    {
+        for (int i = 0; i > liczbaPrzycisków*-30; i -= 30) 
+            {
+            GameObject tempButton = Instantiate(guzik, new Vector3(0, i, 0), Quaternion.identity);
+            tempButton.transform.SetParent(GameObject.FindGameObjectWithTag("ZarzadzaniePraca").transform, false);
+            }
+
+    }
+}
